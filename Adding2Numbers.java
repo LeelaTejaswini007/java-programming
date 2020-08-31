438edf96b46e7972b38e06b6960a3d7a0ff272ce
LeelaTejaswini007 <70505939+LeelaTejaswini007@users.noreply.github.com>
2020-08-31 08:30:48 -0700

    Add files via upload

---
+++ Adding2Numbers.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main{
+           
+        public static void main(String args[]){
+
+                      int input1;
+
+                      int input2;
+
+                      int output;
+
+                      input1=25;
+
+                      input2=31;
+
+                      output=input1+input2;
+
+                      System.out.println(output);
+
+           }
+
+}
+
